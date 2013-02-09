f0bb8cafd3723e4e390a669e9de27981e8780933
b3b <ash.b3b@gmail.com>
2013-02-09 23:35:56 +0400

    Separate thread for the server start

--- ka-lite-android/main.py
+++ ka-lite-android/main.py
@@ -10,35 +10,59 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.clock import Clock
 from kivy.logger import Logger
+import threading
 
 
 class AppLayout(GridLayout):
+    pass
+
+
+class ServerThread(threading.Thread):
     messages = ''
     activities = []
     server_port = '8024'
+
+    def __init__(self, app):
+        super(ServerThread, self).__init__()
+        self.app = app
+
+    def run(self):
+        first_run = False
+        if os.path.exists('ka-lite.zip'):
+            self.schedule('extract_kalite', 'Extracting ka-lite archive')
+            first_run = True
+        self.schedule('setup_environment', 'Setup environment')
+        self.schedule('import_django', 'Try to import Django')
+        self.schedule('syncdb', 'Prepare database')
+        if first_run:
+            self.schedule('generatekeys', 'Generate keys')
+        self.schedule('create_superuser', 'Create admin user')
+        self.schedule('runserver',
+                      "Run server. To see the KA Lite site, open " +
+                      "http://127.0.0.1:{0} in browser".format(
+                self.server_port))
+        self.execute_schedule()
 
     def schedule(self, activity, description):
         self.activities.append((activity, description))
 
     def execute_schedule(self):
-
-        def callback(activity, msg_widget, *args):
-            time.sleep(2)
-            result = activity(args)
-            if result is None:
-                result = 'OK'
-            msg_widget.text = msg_widget.text + result
-            self.execute_schedule()
-
         try:
             activity_name, description = self.activities.pop(0)
         except IndexError:
             pass
         else:
             activity = getattr(self, activity_name)
-            msg_widget = Label(text="{0} ... ".format(description))
-            self.add_widget(msg_widget)
-            Clock.schedule_once(partial(callback, activity, msg_widget))
+            Clock.schedule_once(partial(self.app.report_activity,
+                                        'start', description),
+                                0)
+            result = activity()
+            if result is None:
+                result = 'OK'
+            Clock.schedule_once(partial(self.app.report_activity,
+                                        'result', result),
+                                0)
+        self.execute_schedule()
 
     def extract_kalite(self, *args):
         z = ZipFile('ka-lite.zip', mode="r")
@@ -97,30 +121,25 @@ class AppLayout(GridLayout):
                 'host=127.0.0.1',
                 'threads=3'])
 
-    def run_all(self, *args):
-        first_run = False
-        if os.path.exists('ka-lite.zip'):
-            self.schedule('extract_kalite', 'Extracting ka-lite archive')
-            first_run = True
-        self.schedule('setup_environment', 'Setup environment')
-        self.schedule('import_django', 'Try to import Django')
-        self.schedule('syncdb', 'Prepare database')
-        if first_run:
-            self.schedule('generatekeys', 'Generate keys')
-        self.schedule('create_superuser', 'Create admin user')
-        self.schedule('runserver',
-                      "Run server. To see the KA Lite site, open " +
-                      "http://127.0.0.1:{0} in browser".format(
-                self.server_port))
-        self.execute_schedule()
-
 
 class KALiteApp(App):
 
     def build(self):
-        layout = AppLayout()
-        Clock.schedule_once(layout.run_all, 1)
-        return layout
+        self.layout = AppLayout()
+        return self.layout
+
+    def on_start(self):
+        self.kalite = ServerThread(self)
+        self.kalite.start()
+
+    def report_activity(self, activity, message, *args):
+        assert activity in ('start', 'result')
+        if activity == 'start':
+            self.activity_label = Label(text="{0} ... ".format(message))
+            self.layout.add_widget(self.activity_label)
+        elif hasattr(self, 'activity_label'):
+            self.activity_label.text = self.activity_label.text + message
+
 
 if __name__ == '__main__':
     try:
