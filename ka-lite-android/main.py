88a8e206369ed47ec9c59da57a90a51d92a224dc
b3b <ash.b3b@gmail.com>
2013-02-12 03:55:03 +0400

    Worker thread execution loop ; activities queue

--- ka-lite-android/main.py
+++ ka-lite-android/main.py
@@ -3,6 +3,8 @@ import os
 import time
 from functools import partial
 from zipfile import ZipFile
+import threading
+import Queue
 import kivy
 kivy.require('1.0.7')
 from kivy.uix.gridlayout import GridLayout
@@ -10,7 +12,6 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.clock import Clock
 from kivy.logger import Logger
-import threading
 
 
 class AppLayout(GridLayout):
@@ -18,51 +19,52 @@ class AppLayout(GridLayout):
 
 
 class ServerThread(threading.Thread):
-    messages = ''
-    activities = []
-    server_port = '8024'
 
     def __init__(self, app):
         super(ServerThread, self).__init__()
-        self.app = app
+
+        class AppCaller(object):
+            '''Execute App method in the main thread'''
+
+            def __getattribute__(self, method_name):
+                method = getattr(app, method_name)
+
+                def clock(*args, **kwargs):
+                    Clock.schedule_once(partial(method, *args, **kwargs), 0)
+
+                return clock
+
+        self.app = AppCaller()
+        self._stop_thread = threading.Event()
+        self.activities = Queue.Queue()
 
     def run(self):
-        first_run = False
-        if os.path.exists('ka-lite.zip'):
-            self.schedule('extract_kalite', 'Extracting ka-lite archive')
-            first_run = True
-        self.schedule('setup_environment', 'Setup environment')
-        self.schedule('import_django', 'Try to import Django')
-        self.schedule('syncdb', 'Prepare database')
-        if first_run:
-            self.schedule('generatekeys', 'Generate keys')
-        self.schedule('create_superuser', 'Create admin user')
-        self.schedule('runserver',
-                      "Run server. To see the KA Lite site, open " +
-                      "http://127.0.0.1:{0} in browser".format(
-                self.server_port))
-        self.execute_schedule()
+        '''Execute queue while not stopped'''
 
-    def schedule(self, activity, description):
-        self.activities.append((activity, description))
+        while not self._stop_thread.isSet():
+            try:
+                activity_name, description, args = self.activities.get(True,
+                                                                       4)
+            except Queue.Empty:
+                pass
+            else:
+                activity = getattr(self, activity_name)
+                if description:
+                    self.app.report_activity('start', description)
+                try:
+                    result = activity(*args)
+                except:
+                    self.app.report_activity('result', 'fail')
+                    with self.activities.mutex:
+                        self.activities.queue.clear()
+                    raise
+                if description:
+                    if result is None:
+                        result = 'OK'
+                    self.app.report_activity('result', result)
 
-    def execute_schedule(self):
-        try:
-            activity_name, description = self.activities.pop(0)
-        except IndexError:
-            pass
-        else:
-            activity = getattr(self, activity_name)
-            Clock.schedule_once(partial(self.app.report_activity,
-                                        'start', description),
-                                0)
-            result = activity()
-            if result is None:
-                result = 'OK'
-            Clock.schedule_once(partial(self.app.report_activity,
-                                        'result', result),
-                                0)
-        self.execute_schedule()
+    def schedule(self, activity, description=None, *args):
+        self.activities.put((activity, description, args))
 
     def extract_kalite(self, *args):
         z = ZipFile('ka-lite.zip', mode="r")
@@ -113,10 +115,10 @@ class ServerThread(threading.Thread):
         return 'user "{0}" with password "{1}"'.format(username,
                                                        password)
 
-    def runserver(self, *args):
+    def start_server(self, server_port, *args):
         self.execute_manager(self.settings, [
                 'manage.py', 'runwsgiserver',
-                "port={0}".format(self.server_port),
+                "port={0}".format(server_port),
                 # 'host=0.0.0.0',
                 'host=127.0.0.1',
                 'threads=3'])
@@ -124,12 +126,15 @@ class ServerThread(threading.Thread):
 
 class KALiteApp(App):
 
+    server_port = '8024'
+
     def build(self):
         self.layout = AppLayout()
         return self.layout
 
     def on_start(self):
         self.kalite = ServerThread(self)
+        self.prepare_server()
         self.kalite.start()
 
     def report_activity(self, activity, message, *args):
@@ -139,6 +144,23 @@ class KALiteApp(App):
             self.layout.add_widget(self.activity_label)
         elif hasattr(self, 'activity_label'):
             self.activity_label.text = self.activity_label.text + message
+
+    def prepare_server(self):
+        schedule = self.kalite.schedule
+        first_run = False
+        if os.path.exists('ka-lite.zip'):
+            schedule('extract_kalite', 'Extracting ka-lite archive')
+            first_run = True
+        schedule('setup_environment', 'Setup environment')
+        schedule('import_django', 'Try to import Django')
+        schedule('syncdb', 'Prepare database')
+        if first_run:
+            schedule('generatekeys', 'Generate keys')
+        schedule('create_superuser', 'Create admin user')
+        description = "Run server. To see the KA Lite site, " + (
+            "open  http://127.0.0.1:{0} in browser").format(
+            self.server_port)
+        schedule('start_server', description, self.server_port)
 
 
 if __name__ == '__main__':
