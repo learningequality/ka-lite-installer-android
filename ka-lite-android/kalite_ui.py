31eec1ef80adc3ea31eee77d1fd6ad0b8811893b
66eli77 <66eli77@gmail.com>
2014-10-23 00:06:42 -0700

    comment out all the buttons and input box to give the app a more streamlined look

--- ka-lite-android/kalite_ui.py
+++ ka-lite-android/kalite_ui.py
@@ -33,59 +33,59 @@ class KaliteUI(object):
 
 		self.root_layout.add_widget(logo_houlder)
 
-		#create BubbleButtons
-		btn1= Button(text='OpenBrowser', font_size=30
-		    , color=(0.14, 0.23, 0.25, 1), bold=True)
-		btn1.background_normal='green_button_up.png'
-		#btn1.background_down='button_down.png'
-		btn1.bind(on_press=kaliteApp.start_webview_bubblebutton)
+		# #create BubbleButtons
+		# btn1= Button(text='OpenBrowser', font_size=30
+		#     , color=(0.14, 0.23, 0.25, 1), bold=True)
+		# btn1.background_normal='green_button_up.png'
+		# #btn1.background_down='button_down.png'
+		# btn1.bind(on_press=kaliteApp.start_webview_bubblebutton)
 
-		btn2= Button(text='Exit', font_size=30
-		    , color=(0.14, 0.23, 0.25, 1), bold=True)
-		btn2.background_normal='green_button_up.png'
-		#btn2.background_down='button_down.png'
-		btn2.bind(on_press=kaliteApp.quit_app)
+		# btn2= Button(text='Exit', font_size=30
+		#     , color=(0.14, 0.23, 0.25, 1), bold=True)
+		# btn2.background_normal='green_button_up.png'
+		# #btn2.background_down='button_down.png'
+		# btn2.bind(on_press=kaliteApp.quit_app)
 
-		btn3= Button(text='thread', font_size=30
-		    , color=(0.14, 0.23, 0.25, 1), bold=True)
-		btn3.background_normal='green_button_up.png'
-		#btn3.background_down='button_down.png'
-		btn3.bind(on_press=kaliteApp.set_thread_num)
+		# btn3= Button(text='thread', font_size=30
+		#     , color=(0.14, 0.23, 0.25, 1), bold=True)
+		# btn3.background_normal='green_button_up.png'
+		# #btn3.background_down='button_down.png'
+		# btn3.bind(on_press=kaliteApp.set_thread_num)
 
-		btn4= Button(text='StopServer', font_size=30
-		    , color=(0.14, 0.23, 0.25, 1), bold=True)
-		btn4.background_normal='green_button_up.png'
-		btn4.bind(on_press=kaliteApp.stop_server)
+		# btn4= Button(text='StopServer', font_size=30
+		#     , color=(0.14, 0.23, 0.25, 1), bold=True)
+		# btn4.background_normal='green_button_up.png'
+		# btn4.bind(on_press=kaliteApp.stop_server)
 
-		btn5= Button(text='StartServer', font_size=30
-		    , color=(0.14, 0.23, 0.25, 1), bold=True)
-		btn5.background_normal='green_button_up.png'
-		btn5.bind(on_press=kaliteApp.start_server)
+		# btn5= Button(text='StartServer', font_size=30
+		#     , color=(0.14, 0.23, 0.25, 1), bold=True)
+		# btn5.background_normal='green_button_up.png'
+		# btn5.bind(on_press=kaliteApp.start_server)
 
-		#Add items to bubble
-		buttons_holder = _BoxLayout(orientation='horizontal')
-		buttons_holder.padding = [10,0,10,0]
-		buttons_holder.add_widget(btn1)
-		buttons_holder.add_widget(btn2)
-		buttons_holder.add_widget(btn3)
-		buttons_holder.add_widget(btn4)
-		buttons_holder.add_widget(btn5)
+		# #Add items to bubble
+		# buttons_holder = _BoxLayout(orientation='horizontal')
+		# buttons_holder.padding = [10,0,10,0]
+		# buttons_holder.add_widget(btn1)
+		# buttons_holder.add_widget(btn2)
+		# buttons_holder.add_widget(btn3)
+		# buttons_holder.add_widget(btn4)
+		# buttons_holder.add_widget(btn5)
 
-		self.root_layout.add_widget(buttons_holder)
+		# self.root_layout.add_widget(buttons_holder)
 
 		#image stuff
 		self.img_holder = BoxLayout(orientation='vertical', size=(200,200), size_hint=(1, None))
 		self.img_holder.padding = [0,80,0,10]
 		self.root_layout.add_widget(self.img_holder)
 
-		#thread input box
-		text_input_holder = BoxLayout(orientation='horizontal')
-		text_input_holder.padding = [200,10,200,10]
+		# #thread input box
+		# text_input_holder = BoxLayout(orientation='horizontal')
+		# text_input_holder.padding = [200,10,200,10]
 
-		self.text_input = TextInput(multiline=False, 
-		    hint_text="Enter number of threads here:")
-		self.text_input.padding = [10,10,10,10]
-		text_input_holder.add_widget(self.text_input)
+		# self.text_input = TextInput(multiline=False, 
+		#     hint_text="Enter number of threads here:")
+		# self.text_input.padding = [10,10,10,10]
+		# text_input_holder.add_widget(self.text_input)
 
 		self.progress_bar = ProgressBar()
 
@@ -94,7 +94,7 @@ class KaliteUI(object):
 
 		self.root_layout.add_widget(self.messages)
 		self.root_layout.add_widget(self.server_box)
-		self.root_layout.add_widget(text_input_holder)
+		#self.root_layout.add_widget(text_input_holder)
 		self.root_layout.add_widget(self.progress_bar)
 
 	def get_root_Layout(self):
@@ -114,8 +114,8 @@ class KaliteUI(object):
 	def remove_loading_gif(self):
 		self.img_holder.remove_widget(self.gif_img)
 
-	def get_thread_num(self):
-		return 'threads=' + self.text_input.text
+	# def get_thread_num(self):
+	# 	return 'threads=' + self.text_input.text
 
 	def start_progress_bar(self, anim_value):
 		self.anim = Animation(value = anim_value, duration = 1)
